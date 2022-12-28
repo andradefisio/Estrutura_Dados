6d10b2f7b430d4b14aa12175c92c26edefed97f6
Antonio Andrade <andradefisio97@gmail.com>
2022-12-28 01:25:27 -0300

    Alocação Estática e Dinâmica - Ponteiros

---
+++ semana01/ponteiros_parte2.cpp
@@ -0,0 +1,28 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{	
+	/*
+	//neste código a memória está sendo alocada estaticamente, isto é, em tempo de compilação
+	int a = 10;
+	int *intPointer;
+	intPointer = &a;
+	*intPointer = 50;
+	
+	cout<< "o valor do endereço de memória de interPointer é "<< intPointer << endl;
+	cout<< "interPointer = "<< *intPointer << endl;
+	return 0;
+	*/
+
+	int anotherInt;
+	int *intPointer = new int;
+	*intPointer = 50;
+	anotherInt = *intPointer;
+
+	cout << "O valor de anotherInt será então o valor associado ao endereço de memória para o qual aponta intPointer, ou seja, "<< anotherInt << endl;
+	cout << "intPointer = " << intPointer << ". Este é o valor do endereço de memória para o qual intPointer aponta." <<endl;
+	return 0;
+}
